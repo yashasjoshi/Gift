920a19594108c6befacb1a05c48815d0eac621f9
yashasjoshi <yashasjoshi1996@gmail.com>
2017-05-05 02:54:25 +0530

    The Gift
    This is an augmented reality application which was made as a gift for my
    sister on her birthday.

---
+++ Assets/Scripts/TimerToLoadLevel.js
@@ -0,0 +1,11 @@
+﻿var timer : float = 10.0;
+var level : String = "";
+function Update () 
+{
+    timer -= Time.deltaTime;
+     
+    if(timer <= 0)
+    {
+        Application.LoadLevel(level);
+    }
+}
